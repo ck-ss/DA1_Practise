3a52faf0ca1ec134250c223401359db86ba479e4
samri <samri@_Truffle>
2026-03-28 18:31:32 +0530

    Second commit

--- src/main/java/com/eventshibernate/adad_da1_practisecode/ADAD_DA1_PractiseCode.java
+++ src/main/java/com/eventshibernate/adad_da1_practisecode/ADAD_DA1_PractiseCode.java
@@ -6,5 +6,6 @@ public class ADAD_DA1_PractiseCode {
         System.out.println("23MIC0097");
         System.out.println("Java program using Jenkins CLI");
         System.out.println("Jenkins-GitHub Integration Successful");
+        System.out.println("2nd commit");
     }
 }
